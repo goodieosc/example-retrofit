acdfe2683a24475fcf0eb50866e60176cd1c001e
oscar <oscar.goodwin@gmail.com>
2021-10-29 07:13:55 +0800

    Retrofit added to get results from NASA API

--- app/src/main/java/com/example/example_retrofit/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/example_retrofit/ui/main/MainViewModel.kt
@@ -1,7 +1,38 @@
 package com.example.example_retrofit.ui.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.example_retrofit.ui.main.api.neowsApi
+import retrofit2.Call
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    //Internal property
+    private val _asteriods = MutableLiveData<String>()
+    //External property
+    val asteriods: LiveData<String>
+        get() = _asteriods
+
+    init {
+        getAsteroidList()
+
+    }
+
+
+    private fun getAsteroidList(){
+        neowsApi.retrofitService.getProperties().enqueue(object : retrofit2.Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _asteriods.value = "Failure: " + t.message
+                Log.i("MainViewModel","Failure: ${_asteriods.value.toString()}")
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _asteriods.value = response.body()
+                Log.i("MainViewModel","Success: ${_asteriods.value.toString()}")
+            }
+        })
+    }
 }
--- app/src/main/java/com/example/example_retrofit/ui/main/api/nasaApiService.kt
+++ app/src/main/java/com/example/example_retrofit/ui/main/api/nasaApiService.kt
@@ -1,4 +1,30 @@
 package com.example.example_retrofit.ui.main.api
 
-class nasaApiService {
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://api.nasa.gov/neo/rest/v1/"
+//private var apiParams = "feed?start_date=$startDate&end_date=$endDate&api_key=$apiKey"
+private const val apiParams = "feed?start_date=2021-09-01&end_date=2021-09-07&api_key=U9mndCIzdwnqbnnSEtmWHon1SHywWpkaKRBZsjec"
+
+
+//Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+// Implement the NeoWs [Near Earth Object Web Service] Interface with @Get getProperties returning a String.
+interface neowsApiService {
+    @GET(apiParams) //BASE_URL is appended with apiParams
+    fun getProperties():
+            Call<String>
+
+}
+
+// Create the NeoWs object using Retrofit to implement the NeoWs Service.
+object neowsApi {
+    val retrofitService : neowsApiService by lazy {retrofit.create(neowsApiService::class.java)}
 }
